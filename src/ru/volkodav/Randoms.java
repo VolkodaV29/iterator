1e79e19b82e21ae89f5814457b42a2300a72ccee
Кирилл <93085784+VolkodaV29@users.noreply.github.com>
2022-05-25 15:48:55 +0300

    Add files via upload

---
+++ src/ru/volkodav/Randoms.java
@@ -0,0 +1,34 @@
+package ru.volkodav;
+
+import java.util.Iterator;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    int min;
+    int max;
+
+
+    public Randoms(int min, int max) {
+        random = new Random();
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.ints(min,(max+1)).findAny().getAsInt();
+            }
+        };
+    }
+
+}
